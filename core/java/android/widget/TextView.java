41b0bc242e47258448e8422b782c785f19acdfde
maxwen <max.weninger@gmail.com>
2014-06-06 00:06:28 +0200

    TextView: Check also for mLayoutParams before calling checkForRelayout
    Cherry-picked from OmniROM.
    
    http://jira.omnirom.org/browse/OMNI-976
    
    This was introduced with
    https://github.com/omnirom/android_frameworks_base/commit/b3670c54e60387fde633fb91c649c5b896f8b962
    
    Change-Id: I06c1926c5b8102b336d128d9b411f74a9eff3ded
    
    Prevent NPE in TextView.checkForRelayout
    
    Change-Id: I2b82880080517737f12fba6c582f3b834744b028
    
    Conflicts:
    	core/java/android/widget/TextView.java
    
    Conflicts:
    	core/java/android/widget/TextView.java

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -4057,9 +4057,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
             }
         }
 
-        if (mLayout != null) {
-            checkForRelayout();
-        }
+        checkForRelayout();
 
         sendOnTextChanged(text, 0, oldlen, textLength);
         onTextChanged(text, 0, oldlen, textLength);
@@ -4151,9 +4149,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     public final void setHint(CharSequence hint) {
         mHint = TextUtils.stringOrSpannedString(hint);
 
-        if (mLayout != null) {
-            checkForRelayout();
-        }
+        checkForRelayout();
 
         if (mText.length() == 0) {
             invalidate();
@@ -6828,7 +6824,8 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
         // If we have a fixed width, we can just swap in a new text layout
         // if the text height stays the same or if the view height is fixed.
 
-        if ((mLayoutParams.width != LayoutParams.WRAP_CONTENT ||
+        if ((mLayout != null &&  mLayoutParams != null) &&
+                (mLayoutParams.width != LayoutParams.WRAP_CONTENT ||
                 (mMaxWidthMode == mMinWidthMode && mMaxWidth == mMinWidth)) &&
                 (mHint == null || mHintLayout != null) &&
                 (mRight - mLeft - getCompoundPaddingLeft() - getCompoundPaddingRight() > 0)) {
@@ -8945,9 +8942,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
 
         mTextDir = getTextDirectionHeuristic();
 
-        if (mLayout != null) {
-            checkForRelayout();
-        }
+        checkForRelayout();
     }
 
     TextDirectionHeuristic getTextDirectionHeuristic() {
