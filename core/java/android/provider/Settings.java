54c3fff9679f8f56027f9a2ed19f06c9783db2b7
Jmz <jmztaylor@gmail.com>
2014-06-21 15:15:58 -0400

    Disable Immersive Messages [2/2]
    Based off Xposed module by GermainZ
    
    https://github.com/GermainZ/ImmersiveModeDontPanic
    
    Change-Id: I4835cac08a9e525ec15aa4d1ee84329970ec6a72
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/ImmersiveModeConfirmation.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3425,6 +3425,12 @@ public final class Settings {
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
         /**
+         * Disable Immersive Message
+         * @hide
+         */
+        public static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
