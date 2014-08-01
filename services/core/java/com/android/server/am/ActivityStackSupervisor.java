540d5f07c27ee0b28aeb1264015f203c634c10bc
louis_chang <louis_chang@htc.com>
2014-08-01 10:09:08 +0800

    [ActivityManager] Fix NPE when start activity
    Symptom:
    NPE occurs in line 1184 (resultStack.sendActivityResultLocked)
    because resultStack is null.
    
    Root cause:
    When starting activity with FLAG_ACTIVITY_FORWARD_RESULT flag,
    the resultRecord could be updated, but the resultStack is not
    updated as well. In that case, the resultStack is still be
    null. The exception will occurs if the activity is not
    granted to launch due to permission denied.
    
    Solution:
    Update resultStack when resultRecord updates.
    
    Change-Id: I91634e4f713c2e8dbd1a71f358a8fd9beed83ec7

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1321,7 +1321,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
             }
         }
-        ActivityStack resultStack = resultRecord == null ? null : resultRecord.task.stack;
 
         final int launchFlags = intent.getFlags();
 
@@ -1397,6 +1396,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 err = ActivityManager.START_NOT_VOICE_COMPATIBLE;
             }
         }
+
+        final ActivityStack resultStack = resultRecord == null ? null : resultRecord.task.stack;
 
         if (err != ActivityManager.START_SUCCESS) {
             if (resultRecord != null) {
