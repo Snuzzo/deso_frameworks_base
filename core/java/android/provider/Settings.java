af08e24c030f72eadfd58aab2ff311e274e0585b
dankoman <dankoman30@gmail.com>
2014-12-23 17:57:21 +0100

    [1/2] Frameworks: Doze (ambient display) timeout
    Change-Id: I0eb230020da9a306e089b58db9d48b18c9743c0f
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3192,6 +3192,12 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Timeout for ambient display notification
+         * @hide
+         */
+        public static final String DOZE_TIMEOUT = "doze_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
