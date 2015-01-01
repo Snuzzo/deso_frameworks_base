a002511ea2db4805089b546fefe55899220d1b99
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-01 11:41:00 +0000

    Quick Settings: Auto collapse panel (1/2)
    Since we have some tiles which display detailed view like CellularTile I've
    added the call to the tiles which will use it instead of globally.
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I6d8ecbed72bacfe034f423d5aebf94baa17efff4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3455,6 +3455,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Whether to collapse panel when quick settings tile is pressed.
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_COLLAPSE_PANEL = "quick_settings_collapse_panel";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
