c9ed82a397797e47287f5dc9ab7461a35742da76
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    [1/2] Quick settings: Add Haptic Feedback to tiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2986,6 +2986,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BATTERY_STATUS_COLOR = "status_bar_battery_status_color";
 
+        /** Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
         /**
          * Battery status text color
          * @hide
