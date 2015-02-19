737ebba5a747572386152ecace15744682ed3b8d
Snuzzo <jrracinfan@gmail.com>
2015-02-19 10:05:08 -0500

    1/2 Systemui; Welcome notification on first boot
    Added build properties and status
    
    SystemUI; BootReceiver; Clean-up and fix build

--- packages/SystemUI/src/com/android/systemui/BootReceiver.java
+++ packages/SystemUI/src/com/android/systemui/BootReceiver.java
@@ -16,12 +16,30 @@
 
 package com.android.systemui;
 
+import android.app.Notification;
+import android.app.Notification.Builder;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemProperties;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * Performs a number of miscellaneous, non-system-critical actions
@@ -29,6 +47,7 @@ import android.util.Log;
  */
 public class BootReceiver extends BroadcastReceiver {
     private static final String TAG = "SystemUIBootReceiver";
+    private static String mFirstBootNotify = SystemProperties.get("firstbootnotify");
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -42,5 +61,29 @@ public class BootReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.e(TAG, "Can't start load average service", e);
         }
-    }
+        if (mFirstBootNotify.equals("true")){
+			FirstBootNotify(context);
+		}
+	}
+    
+    public void FirstBootNotify(Context context) {
+		Intent newintent = new Intent();
+		newintent.setClassName("com.android.settings", "com.android.settings.orbix.ChangeLog");
+		PendingIntent contentIntent = PendingIntent.getActivity(context, 0, newintent, PendingIntent.FLAG_ONE_SHOT);
+		Notification.Builder mBuilder = new Notification.Builder(context)
+					.setSmallIcon(R.drawable.first_boot_notify)
+                    .setAutoCancel(true)
+                    .setContentTitle("Welcome to DesolationRom")
+                    .setContentText("Tap to view changelog.")
+					.setContentIntent(contentIntent)
+					.setStyle(new Notification.InboxStyle()
+					.setBigContentTitle("Welcome to DesolationRom")
+					.addLine("Build status: "+SystemProperties.get("rom.buildtype"))
+					.addLine("Build date: "+SystemProperties.get("ro.build.date"))
+					.addLine("Device: "+SystemProperties.get("ro.product.device"))
+					.addLine("Tap to view changelog."));
+		NotificationManager mNotificationManager =
+			(NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(1, mBuilder.build());
+	}
 }
