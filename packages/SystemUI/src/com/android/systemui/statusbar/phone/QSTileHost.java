99548abeb4186df80e118f4d8b57b3aba5c1cc60
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-02-21 22:52:13 +0100

    QSTile: Reboot/Recovery tile (1/2)
    Forward port from SlimKat
    
    Change-Id: Ia933ffd48c485a8a6ca72a3c60139b144ac80e90

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.PowerMenuTile;
 import com.android.systemui.qs.tiles.RoamingTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
@@ -334,6 +335,8 @@ public class QSTileHost implements QSTile.Host {
                 return new BatterySaverTile(this);
             case QSConstants.TILE_SCREEN_OFF:
                 return new ScreenOffTile(this);
+            case QSConstants.TILE_REBOOT:
+                return new RebootTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
