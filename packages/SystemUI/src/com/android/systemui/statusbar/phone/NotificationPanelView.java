932ef2f8be11e3eb7925da13a90b404164051b6d
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-02-28 17:52:58 +0100

    Quick settings customizations, (1/2):
    - Change accent color for SystemUI to deep teal 500, (ff009688)
    - Custom background color
    - Custom icon color
    - Custom text color,
      (this won`t change any text which is using the accent color)
    
    Change-Id: I8ccefddf5eb2d38297f385a7ea83752162a7c563
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/drawable/qs_background_primary.xml
    	packages/SystemUI/res/values/cr_colors.xml
    	packages/SystemUI/src/com/android/systemui/qs/QSDetailItems.java
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    	packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/cr_colors.xml
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Small update to qs colors from @XXMrHyde
    
    Pulled out of here https://github.com/XXMrHyde/android_frameworks_base/commit/0dcf2360f84c769f8c2a45ee1f669acbf41d5e22
    
    Change-Id: I20d61cd6eb9006b2656185c792390d34d70f10bf
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/DataUsageDetailView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -28,6 +28,8 @@ import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.PowerManager;
@@ -192,6 +194,8 @@ public class NotificationPanelView extends PanelView implements
     private int mStatusBarHeaderHeight;
     private GestureDetector mDoubleTapGesture;
 
+    private int mQSBackgroundColor;
+
     public NotificationPanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(!DEBUG);
@@ -268,6 +272,7 @@ public class NotificationPanelView extends PanelView implements
                 }
             }
         });
+        setQSBackgroundColor();
     }
 
     @Override
@@ -2075,6 +2080,12 @@ public class NotificationPanelView extends PanelView implements
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_BACKGROUND_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_ICON_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_TEXT_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_SMART_PULLDOWN),
                     false, this, UserHandle.USER_ALL);
             update();
@@ -2090,6 +2101,18 @@ public class NotificationPanelView extends PanelView implements
 
         public void onChange(boolean selfChange, Uri uri) {
             update();
+            ContentResolver resolver = mContext.getContentResolver();
+            if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_BACKGROUND_COLOR))) {
+                mQSBackgroundColor = Settings.System.getInt(
+                        resolver, Settings.System.QS_BACKGROUND_COLOR, 0xff263238);
+                setQSBackgroundColor();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ICON_COLOR))
+                || uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_TEXT_COLOR))) {
+                setQSColors();
+            }
         }
 
         public void update() {
@@ -2103,6 +2126,29 @@ public class NotificationPanelView extends PanelView implements
             mQsSmartPullDown = Settings.System.getIntForUser(
                     resolver, Settings.System.QS_SMART_PULLDOWN, 0,
                     UserHandle.USER_CURRENT);
+            mQSBackgroundColor = Settings.System.getInt(
+                    resolver, Settings.System.QS_BACKGROUND_COLOR, 0xff263238);
+            setQSBackgroundColor();
+            setQSColors();
+        }
+    }
+
+    private void setQSBackgroundColor() {
+        ContentResolver resolver = mContext.getContentResolver();
+        mQSBackgroundColor = Settings.System.getInt(
+                resolver, Settings.System.QS_BACKGROUND_COLOR, 0xff263238);
+        if (mQsContainer != null) {
+            mQsContainer.getBackground().setColorFilter(
+                    mQSBackgroundColor, Mode.MULTIPLY);
+        }
+        if (mQsPanel != null) {
+            mQsPanel.setDetailBackgroundColor(mQSBackgroundColor);
+        }
+    }
+
+    private void setQSColors() {
+        if (mQsPanel != null) {
+            mQsPanel.setColors();
         }
     }
 }
