4364dd757489ad5ad8ed25ad603c133b8bce07d7
Suresh Koleti <skolet@codeaurora.org>
2015-03-04 15:02:06 +0530

    IMS: Fix add participent and upgrade/downgrade button visibility
    Fix add participent and upgrade/downgrade button visibility
    
    Change-Id: Ie31896787e2c7e18147860e4714e8ae874a486d0
    
    Conflicts:
    	telecomm/java/android/telecom/Call.java
    	telecomm/java/android/telecom/Connection.java

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -174,6 +174,16 @@ public final class Call {
          */
         public static final int CAPABILITY_DISCONNECT_FROM_CONFERENCE = 0x00002000;
 
+        /** 
+         * @hide
+         */
+        public static final int CAPABILITY_CALL_TYPE_MODIFIABLE = 0x00020000;
+
+        /**
+         * @hide
+         */
+        public static final int CAPABILITY_ADD_PARTICIPANT = 0x00080000;
+
         /**
          * Whether the call is a generic conference, where we do not know the precise state of
          * participants in the conference (eg. on CDMA).
@@ -187,6 +197,17 @@ public final class Call {
          * @hide
          */
         public static final int CAPABILITY_SPEED_UP_MT_AUDIO = 0x00008000;
+
+        /** Add participant in an active or conference call option
+         * @hide
+         */
+        public static final int ADD_PARTICIPANT = 0x00008000;
+
+        /**
+         * Call type can be modified for IMS call
+         * @hide
+         */
+        public static final int CALL_TYPE_MODIFIABLE = 0x00020000;
 
         private final Uri mHandle;
         private final int mHandlePresentation;
@@ -274,6 +295,12 @@ public final class Call {
             }
             if (can(capabilities, CAPABILITY_SPEED_UP_MT_AUDIO)) {
                 builder.append(" CAPABILITY_SPEED_UP_IMS_MT_AUDIO");
+            }
+            if (can(capabilities, CALL_TYPE_MODIFIABLE)) {
+                builder.append(" CALL_TYPE_MODIFIABLE");
+            }
+            if (can(capabilities, ADD_PARTICIPANT)) {
+                builder.append(" ADD_PARTICIPANT");
             }
             builder.append("]");
             return builder.toString();
--- telecomm/java/android/telecom/Connection.java
+++ telecomm/java/android/telecom/Connection.java
@@ -204,6 +204,18 @@ public abstract class Connection implements IConferenceable {
     */
     public static final int CAPABILITY_SPEED_UP_MT_AUDIO = 0x00008000;
 
+    /**
+     * Add participant in an active or conference call option
+     * @hide
+     */
+    public static final int ADD_PARTICIPANT = 0x00008000;
+
+    /**
+     * Call type can be modified for IMS call
+     * @hide
+     */
+    public static final int CALL_TYPE_MODIFIABLE = 0x00020000;
+
     // Flag controlling whether PII is emitted into the logs
     private static final boolean PII_DEBUG = Log.isLoggable(android.util.Log.DEBUG);
 
@@ -292,6 +304,12 @@ public abstract class Connection implements IConferenceable {
         }
         if (can(capabilities, CAPABILITY_SPEED_UP_MT_AUDIO)) {
             builder.append(" CAPABILITY_SPEED_UP_IMS_MT_AUDIO");
+        }
+        if (can(capabilities, CALL_TYPE_MODIFIABLE)) {
+            builder.append(" CALL_TYPE_MODIFIABLE");
+        }
+        if (can(capabilities, ADD_PARTICIPANT)) {
+            builder.append(" ADD_PARTICIPANT");
         }
         builder.append("]");
         return builder.toString();
