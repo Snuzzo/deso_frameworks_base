f825fda7c38cdad47bf7cc355abc7ccfdc370ac0
liquid0624 <rjbirnesser@gmail.com>
2015-03-27 20:11:43 -0400

    Revert "[1/2] Frameworks: Doze (ambient display) timeout"
    This reverts commit 9df1fcea61ad7861cee085abc529e431ce768aae.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3039,12 +3039,6 @@ public final class Settings {
         public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
-         * Timeout for ambient display notification
-         * @hide
-         */
-        public static final String DOZE_TIMEOUT = "doze_timeout";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
