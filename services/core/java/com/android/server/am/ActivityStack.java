cdfadc3c34c55912fb39744982162f19f3ed83f9
Steve Kondik <steve@cyngn.com>
2015-03-27 23:24:37 -0700

    Revert QC performance manager, we have our own.
    This reverts commit e9bfde26b69a0346a3d280a2edcbfddc4f0d9d38.
    
    Change-Id: I542bd6cc61b3c0f3c9b665d506a9d966c3344d34
    
    Conflicts:
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -83,7 +83,6 @@ import android.service.voice.IVoiceInteractionSession;
 import android.util.EventLog;
 import android.util.Slog;
 import android.view.Display;
-import com.android.internal.app.ActivityTrigger;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -261,8 +260,6 @@ final class ActivityStack {
     }
 
     final Handler mHandler;
-
-    static final ActivityTrigger mActivityTrigger = new ActivityTrigger();
 
     final class ActivityStackHandler extends Handler {
         //public Handler() {
@@ -1593,8 +1590,6 @@ final class ActivityStack {
 
         if (DEBUG_SWITCH) Slog.v(TAG, "Resuming " + next);
 
-        mActivityTrigger.activityResumeTrigger(next.intent);
-
         // If we are currently pausing an activity, then don't do anything
         // until that is done.
         if (!mStackSupervisor.allPausedActivitiesComplete()) {
@@ -2034,7 +2029,6 @@ final class ActivityStack {
         task.setFrontOfTask();
 
         r.putInHistory();
-        r.info.flags = mActivityTrigger.activityStartTrigger(r.intent, r.info.flags);
         if (!isHomeStack() || numActivities() > 0) {
             // We want to show the starting preview window if we are
             // switching to a new task, or the next activity's process is
