96fa5063eca2c5e3ba557235c9d001a71be1f8c9
Lars Greiss <kufikugel@googlemail.com>
2015-04-06 13:53:41 -0700

    Framework: safe headset volume option (1 of 2)
    thanks DvTonder
    
    Change-Id: I5355820a24d48c1d8f54ea17a7022d891ad07857
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	media/java/android/media/AudioService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2899,6 +2899,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Whether to prevent loud volume levels when headset is first plugged in.
+         * @hide
+         */
+        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
