e533e6c5702c298aa2da818902d9327c56928cac
Andrew Boren <andrew.boren@gmail.com>
2015-04-08 16:08:16 -0700

    Powermenu screenshot delay [1/2]
     * Only works from power menu
     * Defaults to 1 second (1000ms)
    
    Change-Id: Ic7477d4ed962b97d5bbfd980cfdb7e3516262463
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3369,6 +3369,12 @@ public final class Settings {
         public static final String RECENTS_SEARCH_BAR = "recents_search_bar";
 
         /**
+         * Change the screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
