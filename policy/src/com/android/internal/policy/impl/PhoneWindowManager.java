0dfa60cdc6ac1a379457a528707569c3ef00741d
wangjing <wangjing@codeaurora.org>
2015-04-09 16:16:17 +0800

    SystemUI: Set Occluded to false when keyguard is not showing
    When quiting incall view, "Occluded" was not set to false, then
    it fitted system window in keyguard view, so status bar display a
    new black bar.
    
    Set Occluded to false when keyguard is not showing.
    
    Change-Id: Icb87def557e783c61a13f0de752b37324de8c6c0
    CRs-Fixed: 809699

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4453,6 +4453,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mStatusBar.getAttrs().flags &= ~FLAG_SHOW_WALLPAPER;
             return true;
         } else {
+            if (wasOccluded && !isOccluded && !showing) {
+                mKeyguardOccluded = false;
+                mKeyguardDelegate.setOccluded(false);
+            }
             return false;
         }
     }
