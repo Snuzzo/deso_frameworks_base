AICP/frameworks_base

FWB - Add PowerMenu QS Tile (Reboot on longpress) [1/2]
Porting from KK to LP

PS2: whiten icons and add blur

Change-Id: I6e44b91ce14b3c732c990624fba381b8b257f859
Signed-off-by: STELIX <ssspinni@gmail.com>

core/java/com/android/internal/util/cm/QSConstants.java
@@ -38,6 +38,7 @@ public class QSConstants {
     public static final String TILE_APN = "apn";
     public static final String TILE_PROFILES = "profiles";
     public static final String TILE_PERFORMANCE = "performance";
+    public static final String TILE_POWERMENU = "togglePowerMenu";
     public static final String TILE_ADB_NETWORK = "adb_network";
     public static final String TILE_NFC = "nfc";
     public static final String TILE_COMPASS = "compass";
@@ -76,6 +77,7 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_APN);
         TILES_AVAILABLE.add(TILE_PROFILES);
         TILES_AVAILABLE.add(TILE_PERFORMANCE);
+        TILES_AVAILABLE.add(TILE_POWERMENU);
         TILES_AVAILABLE.add(TILE_ADB_NETWORK);
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_COMPASS);

Snuzzo/deso_frameworks_base

Conflicts:
	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

core/java/com/android/internal/util/cm/QSConstants.java
@@ -36,6 +36,7 @@ public class QSConstants {
     public static final String TILE_ROAMING = "roaming";
     public static final String TILE_DDS = "dds";
     public static final String TILE_APN = "apn";
+    public static final String TILE_POWERMENU = "powermenu";
     public static final String TILE_ADB_NETWORK = "adb_network";
     public static final String TILE_NFC = "nfc";
     public static final String TILE_COMPASS = "compass";
@@ -71,6 +72,7 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_ROAMING);
         TILES_AVAILABLE.add(TILE_DDS);
         TILES_AVAILABLE.add(TILE_APN);
+        TILES_AVAILABLE.add(TILE_POWERMENU);
         TILES_AVAILABLE.add(TILE_ADB_NETWORK);
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_COMPASS);