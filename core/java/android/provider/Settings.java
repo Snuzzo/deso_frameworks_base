5a8c5f132a4a05693c5ea0eddefc767ca6586f0b
liquid0624 <rjbirnesser@gmail.com>
2015-05-15 13:16:18 -0400

    Revert "Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)"
    This reverts commit 82578d41a547fc34126e756853c632a73c9b5f13.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3421,12 +3421,6 @@ public final class Settings {
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
 
         /**
-         * Whether the notification light will be allowed when in zen mode during downtime
-         * @hide
-         */
-        public static final String ALLOW_LIGHTS = "allow_lights";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3496,9 +3490,7 @@ public final class Settings {
             NOTIFICATION_SOUND,
             INCREASING_RING,
             INCREASING_RING_START_VOLUME,
-            INCREASING_RING_RAMP_UP_TIME,
-            NONE_IS_SILENT,
-            ALLOW_LIGHTS
+            INCREASING_RING_RAMP_UP_TIME
         };
 
         /**
