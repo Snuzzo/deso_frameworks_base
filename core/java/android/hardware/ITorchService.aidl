CyanogenMod/android_frameworks_base

frameworks: implement system-wide Torch service
Allows Torch and any camera to coexist peacefully. When torch is active
and a camera is opened, the torch will be first shut down, and the
camera will be allowed to continue opening, instead of displaying
"Cannot connect to camera."

Also a squash of the following commits:

    add framework torch service

    Change-Id: I0c7a055b33fdebe697f23d207b20408e06eb74a8
    Signed-off-by: Roman Birg <roman@cyngn.com>

    Fully fix interaction between torch and camera usage.

    For doing this, two things are necessary:
    - The torch shutdown needs to be synchronous, so that the torch app has
      a chance to release the camera before the camera app tries to use it.
    - The torch service needs to track camera usage, so that it can notify
      the torch app if a camera is busy.

    Change-Id: I521091d255ff4c251d97a0e8d65c9d2a80b9dae4

    Fix camera usage tracking.

    When a Camera object is finalized, it notifies the torch service that
    it's no longer using the camera. This call used to not check whether the
    caller actually is still using the camera and called release() before,
    so depending on timing of the finalizer, it could happen that e.g. the
    camera app released the camera user record of the torch app, leading to
    the torch usage not being detected on next usage of the camera app.

    Fix that by checking whether the caller of onCameraClosed() actually
    owned the camera prior to releasing the record.
    Also fix synchronization between accesses.

    Change-Id: Ic01cbc3c89ab8d855e4613bfa1b2f548de919b73

    frameworks: extend TorchService to be able to handle sysfs torch config

    Some devices are still using a sysfs node to control the torch. Let's
    support those configurations and properly shut the torch down.

    Change-Id: I5fa1cccdcffad26dbfef4b2ee0eafe1218a5308b
    Signed-off-by: Roman Birg <roman@cyngn.com>

    frameworks: move TorchService to new services location

    Chan

core/java/android/hardware/ITorchService.aidl
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2014, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware;
+
+import android.hardware.ITorchCallback;
+
+/**
+ * {@hide}
+ */
+interface ITorchService {
+    void onCameraOpened(IBinder token, int cameraId);
+    void onCameraClosed(IBinder token, int cameraId);
+
+    void setTorch(boolean on);
+    void toggleTorch();
+    boolean isAvailable();
+
+    void addListener(ITorchCallback listener);
+    void removeListener(ITorchCallback listener);
+}

Snuzzo/deso_frameworks_base

Conflicts:
	core/java/android/app/ContextImpl.java
	core/res/AndroidManifest.xml
	core/res/res/values/cm_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

core/java/android/hardware/ITorchService.aidl
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2014, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware;
+
+import android.hardware.ITorchCallback;
+
+/**
+ * {@hide}
+ */
+interface ITorchService {
+    void onCameraOpened(IBinder token, int cameraId);
+    void onCameraClosed(IBinder token, int cameraId);
+
+    void setTorchEnabled(boolean on);
+    void toggleTorch();
+    boolean isTorchOn();
+    boolean isAvailable();
+
+    void addListener(ITorchCallback listener);
+    void removeListener(ITorchCallback listener);
+}