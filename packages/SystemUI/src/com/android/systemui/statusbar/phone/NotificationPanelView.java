68a9b98e6cde536f10ed52d181292f9b9f8ee4d3
liquid0624 <rjbirnesser@gmail.com>
2015-05-23 22:08:15 -0400

    SystemUI: do not block quick settings expansion

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -751,8 +751,7 @@ public class NotificationPanelView extends PanelView implements
         }
 
         if ((twoFingerQsEvent || oneFingerQsOverride)
-                && event.getY(event.getActionIndex()) < mStatusBarMinHeight
-                && !isQSEventBlocked) {
+                && event.getY(event.getActionIndex()) < mStatusBarMinHeight) {
             mQsExpandImmediate = true;
             requestPanelHeightUpdate();
 
